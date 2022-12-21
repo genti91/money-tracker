fcf493cdd98fd3b141313908ffc9069950752fca
genti91 <franco.g7@hotmail.com>
2022-12-21 20:21:57 -0300

    mobu

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import Appstyles from './App.scss';
 
 export default function App() {
+  console.log("styles: ",Appstyles);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={Appstyles.container}>
+      <Text>Open up App.js to start workin on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
