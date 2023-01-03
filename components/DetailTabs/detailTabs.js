9df4293a428651e74223d26c83536d18204b6aaa
genti91 <franco.g7@hotmail.com>
2023-01-03 20:36:23 -0300

    details

--- components/DetailTabs/detailTabs.js
+++ components/DetailTabs/detailTabs.js
@@ -1,4 +1,5 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StyleSheet, View, Image } from 'react-native';
 import Home from '../home/home';
 import MonthDetails from '../monthDetails/monthDetails';
 import MonthGraph from '../monthGraph/monthGraph';
@@ -8,14 +9,47 @@ const Tab = createBottomTabNavigator();
 export default function DetailTabs() {
 
 
-
   return (
-    <Tab.Navigator screenOptions={{
+    <Tab.Navigator screenOptions={({route}) => ({
         headerShown: false,
-    }}
+        tabBarIcon: ({focused, color, size}) => {
+
+            return(
+                <View style={focused ? styles.container: ''}>
+                    {route.name === 'Sheet' ? 
+                    <Image source={require('../../assets/sheet.png')} style={{width: 20, height: 20}}/> : 
+                    <Image source={require('../../assets/chart.png')} style={{width: 30, height: 30}}/>
+                    }
+                </View>
+            )
+        },
+        tabBarStyle: {
+            backgroundColor: '#000000',
+            height: 59,
+            borderTopColor: 'transparent',
+        },
+        tabBarShowLabel: false,
+    })}
     >
         <Tab.Screen name="Sheet" component={MonthDetails} />
         <Tab.Screen name="Graph" component={MonthGraph} />
     </Tab.Navigator>
   );
 }
+
+const styles = StyleSheet.create({
+    container: {
+
+        width: 54,
+        height: 54,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#262626',
+        overflow: 'hidden',
+        padding: 0,
+        alignContent: 'center',
+        flexWrap: 'nowrap',
+        gap: 10,
+        borderRadius: 27,
+    },
+});
